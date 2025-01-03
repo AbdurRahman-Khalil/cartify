0d84cc8d26a4ee3fa518fc8da16a7daf2b0d7e4c
AbdurRahman-Khalil <m.abdurrahmankhalil2@gmail.com>
2025-01-03 21:46:51 +0500

    deploy app

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router basename="/">
+    <Router basename="/cartify">
       <App />
     </Router>
   </StrictMode>,
